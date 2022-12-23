3dce40fd32ff9db082885994695b226ee9e5ad36
kosibamarcin <markosv13@gmail.com>
2022-12-23 12:17:04 +0100

    initial

---
+++ Mbed_STM32F413ZHT6/azure_cloud_credentials.h
@@ -0,0 +1,17 @@
+/*
+ * Google Cloud Certificates
+ * Copyright (c) 2019-2020, Arm Limited and affiliates.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef AZURE_CLOUD_CREDENTIALS_H
+#define AZURE_CLOUD_CREDENTIALS_H
+
+namespace azure_cloud {
+namespace credentials {
+/*
+ * Primary Connecion String
+ */
+char iothub_connection_string[] = "HostName=iot-stm-hub.azure-devices.net;DeviceId=stm32f413;SharedAccessKey=y+z9goOQmsxd8P0gD/9BCd0aotwlqao4HSxpUCyfzLg=";
+}
+}
+#endif
