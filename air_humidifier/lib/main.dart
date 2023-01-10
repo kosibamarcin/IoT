b267023f6e6cc8ff9008b63f25ae811231646f32
Patryk Gęgotek <patryk26g@gmail.com>
2023-01-10 16:39:36 +0100

    loading screen

--- air_humidifier/lib/main.dart
+++ air_humidifier/lib/main.dart
@@ -11,8 +11,8 @@ import 'login_page.dart';
 const connectionString =
     'DefaultEndpointsProtocol=https;AccountName=devicemessages12;AccountKey=jfyoNHVw54+5yjC17N2JGFYvknwoUnY9t8YE4UBluHGfuor88+xwlBYOuT/ejuBBeC2MkzoUtkzq+AStZwffPQ==;EndpointSuffix=core.windows.net';
 
-double _humidity = 50.5;
-double _temparature = 44.5;
+double _humidity = 0;
+double _temparature = 0;
 
 void main() {
   runApp(const MyApp());
@@ -25,13 +25,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
-      home: LoginPage()
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+        ),
+        // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: LoginPage());
   }
 }
 
@@ -53,34 +52,56 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  Future<String> getText() async {
+    List<double> list = await testDownloadImage();
+    _humidity = list[0];
+    _temparature = list[1];
+    return '0';
+  }
+
   @override
   Widget build(BuildContext context) {
-    // List<double> list = await testDownloadImage();
+    return FutureBuilder<String>(
+        future: getText(),
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            return mainContent(context);
+          } else {
+            return progress();
+          }
+        });
+  }
+
+  progress() {
+    return Container(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(
+              child: CircularProgressIndicator(),
+              height: 50.0,
+              width: 50.0,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  mainContent(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have clicked the button this many times:',
-            ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              'Data',
+              style: Theme.of(context).textTheme.headline5,
             ),
             const Divider(
               color: Color.fromARGB(255, 223, 223, 223), //color of divider
@@ -105,31 +126,9 @@ class _MyHomePageState extends State<MyHomePage> {
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
-
-// Future<void> testUploadImage() async {
-//   File testFile = File('/home/patryk/Pulpit/cat.png');
-//   Uint8List bytes = testFile.readAsBytesSync();
-//   var storage = AzureStorage.parse(connectionString);
-//   try {
-//     print("Jest ok");
-//     await storage.putBlob(
-//       '/messages/cat.png',
-//       bodyBytes:
-//           bytes, //Text can be uploaded to 'blob' in which case body parameter is specified instead of 'bodyBytes'
-//       contentType: 'image/png',
-//     );
-//   } catch (e) {
-//     print('exception: $e');
-//   }
-// }
 
 Future<List<double>> testDownloadImage() async {
   var storage = AzureStorage.parse(connectionString);
@@ -138,12 +137,10 @@ Future<List<double>> testDownloadImage() async {
     var result =
         await storage.getBlob('/messages/iot-stm-hub/02/2022/12/23/10/58.json');
     var code = result.statusCode;
-    print("Status code: $code");
     var stream = result.stream;
     var data = jsonDecode(await stream.transform(utf8.decoder).join());
     list[0] = double.parse(data[0]["Properties"]["temperature"]);
     list[1] = double.parse(data[0]["Properties"]["humidity"]);
-    print(list);
   } catch (e) {
     print('exception: $e');
   }
